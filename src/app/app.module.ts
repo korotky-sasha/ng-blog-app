6d1009be1ac67625f7eaea2574316c8f62192fd3
korotky sasha <korotky.sasha@gmail.com>
2019-11-15 14:32:19 +0200

    Some code refactoring.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatCardModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
-import { httpInterceptorProviders } from './http-interceptors';
+import { httpInterceptorProviders } from './core/http-interceptors';
 
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth.module';
 import { PostsModule } from './posts/posts.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// @TODO: add different route guards(CanLoad, CanDeactivate...)
 
 @NgModule({
   declarations: [
@@ -24,6 +23,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserAnimationsModule,
     MatButtonModule,
     MatToolbarModule,
+    MatCardModule,
     AuthModule.forRoot(),
     PostsModule,
     AppRoutingModule
